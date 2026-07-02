a4d1fa3d56be686beeb3a43614258ce867dba1d4
async <719939143@qq.com>
2026-07-02 15:38:15 +0800

    添加前缀表达式类型及其解析功能，更新解析器和相关测试用例

--- ast/ast.go
+++ ast/ast.go
@@ -50,6 +50,12 @@ type IntegerLiteral struct{
 	Value int64
 }
 
+type PrefixExpression struct{
+	Token token.Token
+	Operator string
+	Right Expression
+}
+
 func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
@@ -116,6 +122,23 @@ func (il *IntegerLiteral) TokenLiteral() string{
 
 func (il *IntegerLiteral) String() string{
 	return il.Token.Literal
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) String() string{
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+
 }
 
 func (p *Program) TokenLiteral() string {
