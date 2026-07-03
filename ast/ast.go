776c6bd0cfb3e293cc1614a02887aabb72a0b896
async <719939143@qq.com>
2026-07-03 14:56:43 +0800

    添加函数字面量及其解析功能，更新解析器和相关测试用例

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"go_interpreter/token"
+	"strings"
 )
 
 type Node interface {
@@ -78,6 +79,12 @@ type IfExpression struct{
 type BlockStatement struct{
 	Token token.Token
 	Statements []Statement
+}
+
+type FunctionLiteral struct{
+	Token token.Token
+	Parameters []*Identifier
+	Body *BlockStatement
 }
 
 func (ls *LetStatement) statementNode() {}
@@ -242,4 +249,26 @@ func (bs *BlockStatement) String() string{
 	}
 
 	return out.String()
+}
+
+func (fl *FunctionLiteral) expressionNode(){}
+
+func (fl *FunctionLiteral) TokenLiteral() string{
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string{
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters{
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+	return out.String()
+
 }
