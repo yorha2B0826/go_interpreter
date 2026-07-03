d0ed2c94e43562d56b03a1b321ac633d016593c6
async <719939143@qq.com>
2026-07-03 19:34:36 +0800

    添加评估器功能，支持整数和布尔表达式的求值，新增相关测试用例

---
+++ evaluator/evaluator.go
@@ -0,0 +1,43 @@
+package evaluator
+
+import (
+	"go_interpreter/ast"
+	"go_interpreter/object"
+)
+
+var(
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+
+func Eval(node ast.Node) object.Object{
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBooleanObject(node.Value)
+	}
+	return nil
+} 
+
+func evalStatements(stmts []ast.Statement) object.Object{
+	var result object.Object
+	for _, statement := range stmts{
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+func nativeBooleanObject(input bool) *object.Boolean{
+	if input{
+		return TRUE
+	}
+	return FALSE
+}
--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"go_interpreter/evaluator"
 	"go_interpreter/lexer"
 	"go_interpreter/parser"
 	"io"
@@ -24,13 +25,17 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 		p := parser.New(l)
+
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil{
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
