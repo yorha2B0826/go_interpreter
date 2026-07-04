40cd4293b87ff1cf9528ba1bd1cc8975ddbfdfe9
async <719939143@qq.com>
2026-07-04 09:18:27 +0800

    添加比较运算符支持，更新相关测试用例

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -86,6 +86,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch{
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooleanObject(left != right)
 	default:
 		return NULL
 	}
@@ -104,6 +108,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
